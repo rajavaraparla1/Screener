01d127bf8d6624a65d13e126df189110a7925cb1
Nagaraju Varaparla <nagarajuvaraparla@machydnvarapar.local>
2019-04-01 23:30:40 +0530

    Fixed nsetools

--- nsetools/setup.py
+++ nsetools/setup.py
@@ -1,4 +1,3 @@
-import os
 from setuptools import setup, find_packages
 
 readme = '''
@@ -41,14 +40,14 @@ Main Features:
 
 setup(
     name="nsetools",
-    version="1.0.8",
+    version="1.0.10",
     author="Vivek Jha",
     author_email="vsjha18@gmail.com",
-    description = ("Python library for extracting realtime data from National Stock Exchange"),
+    description="Python library for extracting realtime data from National Stock Exchange",
     license="MIT",
     keywords="nse quote market",
     install_requires=['six', 'dateutils'],
-    url ="http://vsjha18.github.com/nsetools",
-    packages = find_packages(),
+    url="http://vsjha18.github.com/nsetools",
+    packages=find_packages(),
     long_description=readme,
 )
